9924d4dc39b463954c98da3c5848ce86ec8a0eed
Nicolas Boada <nicolasboada@yahoo.com.ar>
2022-05-05 13:50:30 -0300

    prettier format

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,101 +1,146 @@
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react'
-import { AiFillEdit, AiOutlineClose, AiOutlineCheck, AiOutlineReload, AiOutlineSearch} from 'react-icons/ai';
-import { BASE_URL } from '../App';
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  AiFillEdit,
+  AiOutlineClose,
+  AiOutlineCheck,
+  AiOutlineReload,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import { BASE_URL } from "../App";
 
-const Header = ({setPinFavorites, setCurrency, editedExRate, setInputExRate,currency,exRateData,wallets,setWallets,fetchWalletData,inputExRate,setExRateData,setEditedExRate,fetchExchangeRates}) => {
+const Header = ({
+  setPinFavorites,
+  setCurrency,
+  editedExRate,
+  setInputExRate,
+  currency,
+  exRateData,
+  wallets,
+  setWallets,
+  fetchWalletData,
+  inputExRate,
+  setExRateData,
+  setEditedExRate,
+  fetchExchangeRates,
+}) => {
+  const [showEdit, setShowEdit] = useState(false);
+  const [inputWallet, setInputWallet] = useState("");
+  const inputExRateRef = useRef("");
 
-const [showEdit, setShowEdit] = useState(false)
-const [inputWallet, setInputWallet] = useState("")
-const inputExRateRef = useRef("")
-
-useEffect(() => {
+  useEffect(() => {
     if (showEdit) {
-    inputExRateRef.current.focus()}
-  }, [showEdit])
+      inputExRateRef.current.focus();
+    }
+  }, [showEdit]);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    if (!inputWallet) return
-    const isInWallets = wallets.find(({address}) => address === inputWallet)
+    e.preventDefault();
+    if (!inputWallet) return;
+    const isInWallets = wallets.find(({ address }) => address === inputWallet);
     if (isInWallets) {
-      alert("Wallet already exists")
-      return
+      alert("Wallet already exists");
+      return;
     }
     try {
-      const newWalletData = await fetchWalletData(inputWallet)
-      const newWallet = await axios.post(`${BASE_URL}/wallets`, newWalletData)
-      setWallets([...wallets, newWallet.data])
-      setInputWallet("")
+      const newWalletData = await fetchWalletData(inputWallet);
+      const newWallet = await axios.post(`${BASE_URL}/wallets`, newWalletData);
+      setWallets([...wallets, newWallet.data]);
+      setInputWallet("");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-  
-const handleEdit = (e) => {
-    e.preventDefault()
-    setExRateData({...exRateData, [currency]: inputExRate})
-    setEditedExRate(true)
-    setShowEdit(false)
-  }
-  
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setExRateData({ ...exRateData, [currency]: inputExRate });
+    setEditedExRate(true);
+    setShowEdit(false);
+  };
 
   return (
     <header className="App-header">
-        <div className='header_top'>
-          <div className='header_title'>
-            Digital Wallet Dashboard
-          </div>
-          <form onSubmit={handleSubmit} className='header_form'>
-            <input 
-              value={inputWallet} 
-              onChange={(e)=>setInputWallet(e.target.value)}
-              placeholder="ETH Wallet Address"
-              type="text"
-              required
-              pattern='^0x[a-fA-F0-9]{40}$'
+      <div className="header_top">
+        <div className="header_title">Digital Wallet Dashboard</div>
+        <form onSubmit={handleSubmit} className="header_form">
+          <input
+            value={inputWallet}
+            onChange={(e) => setInputWallet(e.target.value)}
+            placeholder="ETH Wallet Address"
+            type="text"
+            required
+            pattern="^0x[a-fA-F0-9]{40}$"
+          />
+          <button type="submit">
+            <span>
+              <AiOutlineSearch type="button" />
+            </span>
+          </button>
+        </form>
+      </div>
+      <div className="header_options">
+        <div className="header_options_left">
+          Ether Price:
+          <span> {currency === "USD" ? "$" : "€"} </span>
+          {showEdit ? (
+            <form onSubmit={handleEdit}>
+              <input
+                ref={inputExRateRef}
+                required
+                pattern="^\d+(\.\d+)?$"
+                value={inputExRate}
+                onChange={(e) => setInputExRate(e.target.value)}
               />
-            <button type='submit'>
-              <span >
-                <AiOutlineSearch type='button'/>
-              </span>
-            </button>
-          </form>
+              <AiOutlineCheck
+                className="header_options_left_check_icon"
+                type="submit"
+                onClick={handleEdit}
+              />
+              <AiOutlineClose
+                className="header_options_left_close_icon"
+                type="button"
+                onClick={() => setShowEdit(false)}
+              />
+            </form>
+          ) : (
+            <>
+              <span>{exRateData[currency]}</span>
+              <AiFillEdit
+                className="header_options_left_edit_icon"
+                onClick={() => setShowEdit(true)}
+              />
+              {editedExRate && (
+                <AiOutlineReload
+                  className="header_options_left_reload_icon"
+                  onClick={fetchExchangeRates}
+                />
+              )}
+            </>
+          )}
         </div>
-        <div className='header_options'>
-          <div className='header_options_left'>
-            Ether Price: 
-            <span> {currency === "USD" ? "$" : "€"} </span>
-              {showEdit ?
-                  <form onSubmit={handleEdit}>
-                    <input ref={inputExRateRef} required pattern="^\d+(\.\d+)?$" value={inputExRate} onChange={(e)=>setInputExRate(e.target.value)} />
-                    <AiOutlineCheck className='header_options_left_check_icon' type='submit' onClick={handleEdit}/>
-                    <AiOutlineClose className='header_options_left_close_icon' type='button' onClick={()=>setShowEdit(false)}/>
-                  </form>
-                :
-                <>
-                <span>{exRateData[currency]}</span>
-                <AiFillEdit className='header_options_left_edit_icon' onClick={()=>setShowEdit(true)}/>
-                {editedExRate && <AiOutlineReload className='header_options_left_reload_icon' onClick={fetchExchangeRates}/>}
-                </>
-              }
+        <div className="header_options_right">
+          <div>
+            <label htmlFor="">Select currency</label>
+            <select
+              name="currency"
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+            </select>
           </div>
-          <div className='header_options_right'>
-            <div>
-              <label htmlFor="">Select currency</label>
-              <select name="currency" onChange={(e)=>setCurrency(e.target.value)}>
-                <option value="USD" >USD</option>
-                <option value="EUR" >EUR</option>
-              </select>
-            </div>
-            <div>
-              <label >Pin favorites</label>
-              <input type="checkbox" onChange={(e) => setPinFavorites(e.target.checked)}/>
-            </div>
+          <div>
+            <label>Pin favorites</label>
+            <input
+              type="checkbox"
+              onChange={(e) => setPinFavorites(e.target.checked)}
+            />
           </div>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Components/Wallet.jsx
+++ src/Components/Wallet.jsx
@@ -1,64 +1,92 @@
-import axios from 'axios';
-import React from 'react'
-import { AiFillStar, AiOutlineClose, AiOutlineInfoCircle, AiOutlineStar } from 'react-icons/ai';
-import { BASE_URL } from '../App';
+import axios from "axios";
+import React from "react";
+import {
+  AiFillStar,
+  AiOutlineClose,
+  AiOutlineInfoCircle,
+  AiOutlineStar,
+} from "react-icons/ai";
+import { BASE_URL } from "../App";
 
-const Wallet = ({wallet,currency,exRateData,wallets,setWallets}) => {
-    
-    const toggleFav = async(id) => {
-        try {
-            const selectedWallet = wallets.find((wallet) => wallet._id === id);
-            const updatedWallet = {...selectedWallet, isFavorite: !selectedWallet.isFavorite}
-            await axios.put(`${BASE_URL}/wallets/${id}`, updatedWallet)
-            setWallets(wallets.map(wallet => wallet._id === id ? {...wallet, isFavorite: !wallet.isFavorite} : wallet) )
-        } catch (error) {
-            console.log(error)
-        }
+const Wallet = ({ wallet, currency, exRateData, wallets, setWallets }) => {
+  const toggleFav = async (id) => {
+    try {
+      const selectedWallet = wallets.find((wallet) => wallet._id === id);
+      const updatedWallet = {
+        ...selectedWallet,
+        isFavorite: !selectedWallet.isFavorite,
+      };
+      await axios.put(`${BASE_URL}/wallets/${id}`, updatedWallet);
+      setWallets(
+        wallets.map((wallet) =>
+          wallet._id === id
+            ? { ...wallet, isFavorite: !wallet.isFavorite }
+            : wallet
+        )
+      );
+    } catch (error) {
+      console.log(error);
     }
-        
-    const handleDelete = async(id) => {
-        try {
-            await axios.delete(`${BASE_URL}/wallets/${id}`);
-            setWallets(wallets.filter(wallet => wallet._id !== id))
-        } catch (error) {
-            console.log(error)
-        }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${BASE_URL}/wallets/${id}`);
+      setWallets(wallets.filter((wallet) => wallet._id !== id));
+    } catch (error) {
+      console.log(error);
     }
-        
+  };
+
   return (
-        <div className='wallet_card'>
-            <div className='wallet_card_header'>
-                <div>
-                Address <span>{wallet.address}</span>
-                </div>
-                <div>
-                {wallet.isFavorite ?
-                <AiFillStar className='wallet_card_header_fillStar_icon' onClick={()=>toggleFav(wallet._id)}/> :
-                <AiOutlineStar className='wallet_card_header_outlineStar_icon' onClick={()=>toggleFav(wallet._id)}/>}
-                <AiOutlineClose className='wallet_card_header_close_icon' onClick={()=>handleDelete(wallet._id)}/>
-                </div>                
-            </div>
-            <div className='wallet_card_row'>
-                <span>Balance: </span>
-                <span>{wallet.balance} Ether</span>
-            </div>
-            <div className='wallet_card_row'>
-                <span>Value: </span>
-                <span>{currency === "USD" ? "$" : "€"} </span>
-                <span>{ (wallet.balance * exRateData[currency]).toFixed(2) }</span>
-                <span className='wallet_card_small'> (@ {currency === "USD" ? "$" : "€"}{exRateData[currency]}/ETH)</span> 
-            </div>
-            {wallet.isOld &&
-            <>
-            <div className='wallet_card_row'>
-                <div className='wallet_card_info_old'>
-                <AiOutlineInfoCircle /> wallet is old! 
-                </div>
-            </div>
-            </>
-            } 
+    <div className="wallet_card">
+      <div className="wallet_card_header">
+        <div>
+          Address <span>{wallet.address}</span>
         </div>
-  )
-}
+        <div>
+          {wallet.isFavorite ? (
+            <AiFillStar
+              className="wallet_card_header_fillStar_icon"
+              onClick={() => toggleFav(wallet._id)}
+            />
+          ) : (
+            <AiOutlineStar
+              className="wallet_card_header_outlineStar_icon"
+              onClick={() => toggleFav(wallet._id)}
+            />
+          )}
+          <AiOutlineClose
+            className="wallet_card_header_close_icon"
+            onClick={() => handleDelete(wallet._id)}
+          />
+        </div>
+      </div>
+      <div className="wallet_card_row">
+        <span>Balance: </span>
+        <span>{wallet.balance} Ether</span>
+      </div>
+      <div className="wallet_card_row">
+        <span>Value: </span>
+        <span>{currency === "USD" ? "$" : "€"} </span>
+        <span>{(wallet.balance * exRateData[currency]).toFixed(2)}</span>
+        <span className="wallet_card_small">
+          {" "}
+          (@ {currency === "USD" ? "$" : "€"}
+          {exRateData[currency]}/ETH)
+        </span>
+      </div>
+      {wallet.isOld && (
+        <>
+          <div className="wallet_card_row">
+            <div className="wallet_card_info_old">
+              <AiOutlineInfoCircle /> wallet is old!
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default Wallet
+export default Wallet;
